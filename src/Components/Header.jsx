57b43d80b24fd3d2e15f29ea07a412376567c3be
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-10 23:27:35 +0100

    Added buttons to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import image from "../assets/react.svg";
 
 const Header = () => {
   return (
-    <div className="text-3xl py-6 px-6 text-blue-500 justify-around flex flex-row gap-5 "></div>
+    <main className="text-3xl py-6 px-6 text-blue-500 justify-around flex flex-row gap-5 ">
+      <div>
+        <img src={image} alt="The Image" />
+      </div>
+
+      <div>
+        {" "}
+        <button>Enterprise</button>
+        <button>Pricing </button>
+        <button>Explore</button>
+        <button>Plans</button>
+      </div>
+
+      <div>
+        <button></button>
+      </div>
+    </main>
   );
 };
 
