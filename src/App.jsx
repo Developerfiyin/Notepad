62b10f85223089376871fd2c8aeedb26575f8e21
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-10 22:17:29 +0100

    Created a header.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Header from "./Components/header";
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <Header />
+      </div>
     </>
   );
 }
