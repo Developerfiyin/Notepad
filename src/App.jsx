c76f5dca290d9750603274c19b0a6edfc533c210
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-10 20:44:39 +0100

    Installed tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <> </>;
+  return (
+    <>
+      <div></div>
+    </>
+  );
 }
 
 export default App;
