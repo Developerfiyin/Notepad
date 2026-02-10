8e2181f0a978f9830673fbb7eb6d51b74961e90f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-10 22:28:09 +0100

    styled global css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="text-3xl py-6 px-6 text-blue-500 justify-around flex flex-row gap-5 "></div>
+  );
 };
 
 export default Header;
