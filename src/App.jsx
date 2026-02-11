43202769eba5271b6f6b6f356393d02915741925
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-11 11:23:18 +0100

    Deleted dashboard.jsx because of some arising conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <div>
+      <div> 
         <Routes>
           <Route path="/" element={<Header />} /> {/* Header page*/}
           <Route path="/account" element={<Account />} /> {/* Login page*/}
