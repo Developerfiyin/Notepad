4e036f9ba0b6ee3431f729c005675cac6c34feb9
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-11 12:51:50 +0100

    Replaced the flex and gap with space-x in all the buttons div

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,12 +10,12 @@ const Header = () => {
   };
   return (
     <nav className="sticky top-0 z-50 w-full bg-white border-b border-gray-200 py-4">
-      <div>
+      <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
         <div>
           <img src={image} alt="The Image" />
         </div>
 
-        <div className="flex gap-3 font-bold ">
+        <div className="space-x-4 font-bold ">
           <button className="bg-purple-500 px-2 rounded-md hover:text-blue-500 ease-in-out duration-300 ">
             Enterprise
           </button>
@@ -30,7 +30,7 @@ const Header = () => {
           </button>
         </div>
 
-        <div className="flex gap-3 font-bold">
+        <div className=" space-x-4 font-bold">
           <button className="bg-purple-500 px-2 rounded-md hover:text-blue-500 ease-in-out duration-300">
             Log in
           </button>
