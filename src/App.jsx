c55c8ffce3264c1a806d4105fe96b170898b1749
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-11 10:18:31 +0100

    Updated Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import Header from "./Components/header";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <div>
+        <Routes>
+          <Route path="/" element={<Header />} /> {/* Header page*/}
+        </Routes>
         <Header />
       </div>
     </>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,21 +3,33 @@ import image from "../assets/react.svg";
 
 const Header = () => {
   return (
-    <main className="text-3xl py-6 px-6 text-blue-500 justify-around flex flex-row gap-5 ">
+    <main className="text-xl font-medium py-6 px-6 mx-auto text-white justify-around flex flex-row ">
       <div>
         <img src={image} alt="The Image" />
       </div>
 
-      <div>
-        {" "}
-        <button>Enterprise</button>
-        <button>Pricing </button>
-        <button>Explore</button>
-        <button>Plans</button>
+      <div className="flex gap-3 font-bold ">
+        <button className="bg-purple-500 px-2 rounded-md hover:text-blue-500 ease-in-out duration-300 ">
+          Enterprise
+        </button>
+        <button className="bg-purple-500 px-2 rounded-md hover:text-blue-500 ease-in-out duration-300">
+          Pricing
+        </button>
+        <button className="bg-purple-500 px-2 rounded-md hover:text-blue-500 ease-in-out duration-300">
+          Explore
+        </button>
+        <button className="bg-purple-500 px-2 rounded-md hover:text-blue-500 ease-in-out duration-300">
+          Plans
+        </button>
       </div>
 
-      <div>
-        <button></button>
+      <div className="flex gap-3 font-bold">
+        <button className="bg-purple-500 px-2 rounded-md hover:text-blue-500 ease-in-out duration-300">
+          Log in
+        </button>
+        <button className="bg-purple-500 px-2 rounded-md hover:text-blue-500 ease-in-out duration-300">
+          Account
+        </button>
       </div>
     </main>
   );
