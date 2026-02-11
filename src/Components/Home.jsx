248566aa2420140f1a76adc2fa71276370ce86a0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-11 19:09:59 +0100

    Added a div containing some texts

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./header";
-import image from "../assets/image.png";
+import image from "../assets/image1.png";
+import image1 from "../assets/image2.png";
 const Home = () => {
   return (
     <main>
       <Header />
-      <div className="bg-white py-6  grid px-12 w-full ">
-        <div className="bg-black/65 p-5">
+      <div className="bg-white py-6 items-center grid px-12 w-full ">
+        <div className="bg-black/65 p-5 px-5 rounded-2xl mb-12">
           <h1 className="text-7xl font-medium mt-2 text-center mb-2">
             Get more from your note- <br className="mb-4" />
             taking app
@@ -16,10 +17,24 @@ const Home = () => {
             productive.
           </h2>
           <button className="text-white text-xl hover:bg-black/40 py-3 px-4 mb-5 rounded-md ml-120 bg-black ">
-            Get note taker for windows
+            Get Notetaker free
           </button>
-
           <img src={image} alt="" className="" />
+        </div>
+
+        <div className="flex-row text-center justify-center bg-red-600 rounded-2xl flex">
+          <img src={image1} alt="The second image for the home section" />
+          <div className=" my-auto  ">
+            <h2 className="text-4xl font-bold text-center">
+              Remember <br className="" /> Everything
+            </h2>
+
+            <h4>
+              Capture thoughts and inspiration anywhere, at any time, in any
+              format, and on any device—your important notes, images, receipts,
+              and documents are safe and right at your fingertips.
+            </h4>
+          </div>
         </div>
       </div>
     </main>
