73f4cc3b7b41c285991ceff9bc314f0d5a53a5d5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-11 18:26:46 +0100

    Updated divs for home.jsx file

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
   return (
     <main>
       <Header />
-      <div className="bg-white py-6 px-12 w-full ">
-        <div className="bg-black/65">
-          <h1 className="text-7xl font-medium text-center mb-2">
+      <div className="bg-white py-6  grid px-12 w-full ">
+        <div className="bg-black/65 p-5">
+          <h1 className="text-7xl font-medium mt-2 text-center mb-2">
             Get more from your note- <br className="mb-4" />
             taking app
           </h1>
@@ -15,7 +15,11 @@ const Home = () => {
             Capture important ideas and information in ways that help you stay
             productive.
           </h2>
-          <img src={image} alt="" />
+          <button className="text-white text-xl hover:bg-black/40 py-3 px-4 mb-5 rounded-md ml-120 bg-black ">
+            Get note taker for windows
+          </button>
+
+          <img src={image} alt="" className="" />
         </div>
       </div>
     </main>
