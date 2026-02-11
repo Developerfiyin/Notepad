db4a0261693b8592802247b5c08d8d73c646b29c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-11 17:47:32 +0100

    Updated div

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,16 +5,18 @@ const Home = () => {
   return (
     <main>
       <Header />
-      <div className="bg-white  py-6 px-8 w-full ">
-        <h1 className="text-7xl font-medium text-center mb-2">
-          Get more from your note- <br className="mb-3" />
-          taking app
-        </h1>
-        <h2 className="text-xl mt-6 font-medium text-center mb-12">
-          Capture important ideas and information in ways that help you stay
-          productive.
-        </h2>
-        <img src={image} alt="" className="w-full " />
+      <div className="bg-white py-6 px-12 w-full ">
+        <div className="bg-black/65">
+          <h1 className="text-7xl font-medium text-center mb-2">
+            Get more from your note- <br className="mb-4" />
+            taking app
+          </h1>
+          <h2 className="text-xl mt-6 font-medium text-center mb-12">
+            Capture important ideas and information in ways that help you stay
+            productive.
+          </h2>
+          <img src={image} alt="" />
+        </div>
       </div>
     </main>
   );
